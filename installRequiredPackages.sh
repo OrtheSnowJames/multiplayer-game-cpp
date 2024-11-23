29c0ab452ca2a2465d77f1e6683ec81c8b16f39f
James On Arch <jamesyacoube@yahoo.com>
2024-11-22 16:48:46 -0800

    yes

--- installRequiredPackages.sh
+++ installRequiredPackages.sh
@@ -1,9 +1,14 @@
 #put apt packages here
+read -p "apt or pacman" packagemanager
+if [packagemanager == "apt"]; then
 sudo apt install build-essential git
 sudo apt install libboost-all-dev
 sudo apt install nlohmann-json3-dev
 sudo apt install libraylib-dev
 sudo apt install xterm
+fi
+else then
+fi
 #put git packages here
 cd /home/
 git clone https://github.com/raysan5/raylib
